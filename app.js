378e9587646f308aca499f3b6167266d2739c804
AChareun <achareun22@gmail.com>
2020-06-25 10:17:57 -0300

    update app.js to work with routes dir

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ const express = require('express');
 
 const exphbs = require('express-handlebars');
 
+const bodyParser = require('body-parser');
+
+const path = require('path');
+
 const PORT = 8080;
 const app = express();
+
+const router = require('./routes/handlers.js');
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`server listening at ${PORT}`);
+});
