f86883c594f4360ce53d0f085ebc83e4c274e16e
AChareun <achareun22@gmail.com>
2020-06-26 17:43:51 -0300

    add delete method for clubs

--- routes/club-page.js
+++ routes/club-page.js
@@ -1,7 +1,13 @@
+const file = require('../public/js/RW-helpers');
+
 const clubPageRoutes = (app, fs) => {
-  app.get('/club/:id', (req, res) => {
-    const dataPath = `./public/data/equipos/${req.params.id}.json`;
-    fs.readFile(dataPath, 'utf8', (err, data) => {
+  const dataPath = './public/data/equipos.json';
+  const teamPath = './public/data/equipos/';
+
+  // Refactor to use IDs instead of TLA
+
+  app.get('/club/:tla', (req, res) => {
+    fs.readFile(`${teamPath + req.params.tla}.json`, 'utf8', (err, data) => {
       if (err) {
         throw err;
       }
@@ -13,7 +19,22 @@ const clubPageRoutes = (app, fs) => {
         style: 'club.css',
       });
     });
-  });
+  })
+    .delete('/club/:tla/delete', (req, res) => {
+      file.readFile(fs, dataPath, (data) => {
+        const newData = data;
+        const clubToDelete = newData.find((team) => team.tla === req.params.tla);
+        delete newData[newData.indexOf(clubToDelete)];
+
+        // Delete leaves a null on the JSON. Get rid of it
+
+        // Still needs to delete the standalone team file
+
+        file.writeFile(fs, dataPath, JSON.stringify(newData, null, 2), () => {
+          res.status(200).redirect('/club-list');
+        });
+      }, true);
+    });
 };
 
 module.exports = clubPageRoutes;
